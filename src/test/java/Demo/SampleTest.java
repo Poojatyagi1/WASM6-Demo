29f3007e78da9612d33c168f8b269f6fdb102ca2
Poojatyagi1 <Poojashaileshtyagi@gmail.com>
2022-11-24 10:48:19 +0530

    commit6

--- src/test/java/Demo/SampleTest.java
+++ src/test/java/Demo/SampleTest.java
@@ -1,14 +1,15 @@
-package demo;
+package Demo;
 
 import org.testng.annotations.Test;
 
 public class SampleTest 
 {
-	
 	@Test
-	public void sample()
+	public static void sample()
 	{
-		System.out.println("This is sample class");
+		
+	System.out.println("Hi this is WASM6 Framework");	
+
 	}
 
 }
